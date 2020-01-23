2454d263a4947d6f978eb2c1a3f2ecee606f70b5
Ayush Dubey <2017UGCS079@nitjsr.ac.in>
2020-01-24 01:06:48 +0530

    Modify data

--- scripts/modifyTrainNumbers.py
+++ scripts/modifyTrainNumbers.py
@@ -1,2 +1,11 @@
 import csv
-f = open("../datasets/trains.csv","r")
+with open("../datasets/trains.csv","r") as readFile :
+	file = csv.reader(readFile)
+	i=0
+	for row in file:
+		i=i+1
+		if i==1:
+			continue
+		print(row[0])
+		f = open("../datasets/modifiedTrainNumbers.txt","a")
+		f.write(row[0]+"\n")
