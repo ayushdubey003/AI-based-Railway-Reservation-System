d6d35f152245707861c900058f887f2cb9059e27
Ayush Dubey <2017UGCS079@nitjsr.ac.in>
2020-01-31 17:02:06 +0530

    Modify API

--- APIs/app.py
+++ APIs/app.py
@@ -23,55 +23,91 @@ def stationslistversion():
 def level(code):
     return jsonify(name=code)
 
-
 @app.route("/trains/<board>/<destination>", methods=['GET'])
 def listoftrains(board, destination):
     ans = []
-    with open('../datasets/trains.csv', 'rt') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            flag = 0
-            med_stations = row[7]
-            sep_stations = med_stations.split('$')
-            for med in sep_stations:
-                if med == board:
-                    if flag == 0:
-                        flag += 1
-                if med == destination:
-                    if flag == 1:
-                        flag += 1
-                if flag == 2:
-                    classes = row[3].split("\u00a0")
-                    arr = row[8].split("$")
-                    dept = row[9].split("$")
-                    namedRoutes = row[6].split("$")
-                    codedRoutes = row[7].split("$")
-                    days = row[2].split(" ")
-                    rt = row[10].split("$")
-                    sld = row[11].split("$")
-                    sd = row[12].split("$")
-                    can = row[13].split("$")
+    if destination == "null":
+        with open("../datasets/trains.csv",'rt') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                flag = 0
+                med_stations = row[7]
+                sep_stations = med_stations.split('$')
+                for med in sep_stations:
+                    if med == board:
+                        classes = row[3].split("\u00a0")
+                        arr = row[8].split("$")
+                        dept = row[9].split("$")
+                        namedRoutes = row[6].split("$")
+                        codedRoutes = row[7].split("$")
+                        days = row[2].split(" ")
+                        rt = row[10].split("$")
+                        sld = row[11].split("$")
+                        sd = row[12].split("$")
+                        can = row[13].split("$")
 
-                    ans.append({
-                            "train no": row[0],
-                            "train name": row[1],
-                            "running days": days,
-                            "available classes": classes,
-                            "type": row[4],
-                            "zone": row[5],
-                            "route with names": namedRoutes,
-                            "route with codes": codedRoutes,
-                            "arrival times": arr,
-                            "departure times": dept,
-                            "right time" : rt,
-                            "slight delay" : sld,
-                            "significant delay" : sd,
-                            "cancelled/unknown" : can
-                        })
-                    break
+                        ans.append({
+                                "train no": row[0],
+                                "train name": row[1],
+                                "running days": days,
+                                "available classes": classes,
+                                "type": row[4],
+                                "zone": row[5],
+                                "route with names": namedRoutes,
+                                "route with codes": codedRoutes,
+                                "arrival times": arr,
+                                "departure times": dept,
+                                "right time" : rt,
+                                "slight delay" : sld,
+                                "significant delay" : sd,
+                                "cancelled/unknown" : can
+                            })
+                        break
+    else:
+        with open('../datasets/trains.csv', 'rt') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                flag = 0
+                med_stations = row[7]
+                sep_stations = med_stations.split('$')
+                for med in sep_stations:
+                    if med == board:
+                        if flag == 0:
+                            flag += 1
+                    if med == destination:
+                        if flag == 1:
+                            flag += 1
+                    if flag == 2:
+                        classes = row[3].split("\u00a0")
+                        arr = row[8].split("$")
+                        dept = row[9].split("$")
+                        namedRoutes = row[6].split("$")
+                        codedRoutes = row[7].split("$")
+                        days = row[2].split(" ")
+                        rt = row[10].split("$")
+                        sld = row[11].split("$")
+                        sd = row[12].split("$")
+                        can = row[13].split("$")
+
+                        ans.append({
+                                "train no": row[0],
+                                "train name": row[1],
+                                "running days": days,
+                                "available classes": classes,
+                                "type": row[4],
+                                "zone": row[5],
+                                "route with names": namedRoutes,
+                                "route with codes": codedRoutes,
+                                "arrival times": arr,
+                                "departure times": dept,
+                                "right time" : rt,
+                                "slight delay" : sld,
+                                "significant delay" : sd,
+                                "cancelled/unknown" : can
+                            })
+                        break
 
     return jsonify(trains=ans)
-
 
 @app.route("/list", methods=['GET'])
 def listofstations():
