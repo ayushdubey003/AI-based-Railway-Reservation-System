e5171428f118916cc4edbc75611aa56dca0f3abc
Ayush Dubey <2017UGCS079@nitjsr.ac.in>
2020-02-02 16:31:22 +0530

    API update: add seat availability status

--- APIs/app.py
+++ APIs/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify
+import requests 
+from bs4 import BeautifulSoup
 import csv
 app = Flask(__name__)
 
@@ -23,10 +25,10 @@ def stationslistversion():
 def level(code):
     return jsonify(name=code)
 
-@app.route("/trains/<board>/<destination>", methods=['GET'])
-def listoftrains(board, destination):
-    ans = []
-    if destination == "null":
+@app.route("/trains/<board>", methods=['GET'])
+def listoftrainssourceindependent(board):
+    ans=[]
+    if(True):
         with open("../datasets/trains.csv",'rt') as file:
             reader = csv.reader(file)
             for row in reader:
@@ -63,7 +65,13 @@ def listoftrains(board, destination):
                                 "cancelled/unknown" : can
                             })
                         break
-    else:
+
+    return jsonify(trains=ans)
+
+@app.route("/trains/<board>/<destination>", methods=['GET'])
+def listoftrains(board, destination):
+    ans = []
+    if(True):
         with open('../datasets/trains.csv', 'rt') as file:
             reader = csv.reader(file)
             for row in reader:
@@ -108,6 +116,83 @@ def listoftrains(board, destination):
                         break
 
     return jsonify(trains=ans)
+
+@app.route("/trains/<board>/<destination>/<doj>", methods=['GET'])
+def listoftrainsdaywise(board, destination,doj):
+    ans = []
+
+    if(True):
+        with open('../datasets/trains.csv', 'rt') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                flag = 0
+                med_stations = row[7]
+                sep_stations = med_stations.split('$')
+                for med in sep_stations:
+                    if med == board:
+                        if flag == 0:
+                            flag += 1
+                    if med == destination:
+                        if flag == 1:
+                            flag += 1
+                    if flag == 2:
+                        classes = row[3].split("\u00a0")
+                        arr = row[8].split("$")
+                        dept = row[9].split("$")
+                        namedRoutes = row[6].split("$")
+                        codedRoutes = row[7].split("$")
+                        days = row[2].split(" ")
+                        rt = row[10].split("$")
+                        sld = row[11].split("$")
+                        sd = row[12].split("$")
+                        can = row[13].split("$")
+                        z= False
+                        for day in days:
+                            if day.lower() == doj.lower():
+                                z=True
+                                break
+
+                        if((len(days) == 1 and days[0].lower() == "daily") or z):
+                            ans.append({
+                                    "train no": row[0],
+                                    "train name": row[1],
+                                    "running days": days,
+                                    "available classes": classes,
+                                    "type": row[4],
+                                    "zone": row[5],
+                                    "route with names": namedRoutes,
+                                    "route with codes": codedRoutes,
+                                    "arrival times": arr,
+                                    "departure times": dept,
+                                    "right time" : rt,
+                                    "slight delay" : sld,
+                                    "significant delay" : sd,
+                                    "cancelled/unknown" : can
+                                })
+                        break
+
+    return jsonify(trains=ans)
+
+@app.route("/seats/<trainno>/<board>/<destination>/<travelClass>/<doj>/<quota>", methods=['GET'])
+def seat_availability(trainno, board, destination, travelClass, doj, quota):
+    url = "https://www.railyatri.in/seat-availability/" +trainno+"-"+board+"-"+destination+"?journey_class="+travelClass+"&journey_date="+doj+"&quota="+quota
+    try:
+        content = requests.get(url)
+        soup = BeautifulSoup(content.text, 'html.parser')
+
+        try:
+            print(url)
+            availability = []
+            x = soup.find('div',{"id":"one"}).find_all('div',{"class":"seat_availability_details"})
+            for i in range(0,len(x)):
+                v = "first"+str(i+1)
+                availability.append(x[i].find('span',{"class":"status_one"}).find('div',{"id":v}).text)
+            return jsonify(seatavailability=availability)
+        except Exception as e:
+            return jsonify(error = str(e))
+
+    except Exception as e:
+        return jsonify(error = str(e))
 
 @app.route("/list", methods=['GET'])
 def listofstations():
