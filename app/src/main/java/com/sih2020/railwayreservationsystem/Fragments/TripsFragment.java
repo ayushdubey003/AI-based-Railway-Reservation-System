6c96a166e8d49c8304f9a0b4f93ad11038f90f23
tachodril <rajlevel97@gmail.com>
2020-02-04 00:38:06 +0530

    pnr fragment layout

--- app/src/main/java/com/sih2020/railwayreservationsystem/Fragments/TripsFragment.java
+++ app/src/main/java/com/sih2020/railwayreservationsystem/Fragments/TripsFragment.java
@@ -1,18 +1,26 @@
 package com.sih2020.railwayreservationsystem.Fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.sih2020.railwayreservationsystem.Activities.PnrActivity;
 import com.sih2020.railwayreservationsystem.R;
 
 public class TripsFragment extends Fragment {
+
+    private Button find_pnr_button;
+
     public TripsFragment() {
         // Required empty public constructor
     }
@@ -32,5 +40,21 @@ public class TripsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_trips, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        init(view);
+        find_pnr_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), PnrActivity.class));
+            }
+        });
+    }
+
+    private void init(View view) {
+        find_pnr_button = view.findViewById(R.id.find_pnr_button);
     }
 }
