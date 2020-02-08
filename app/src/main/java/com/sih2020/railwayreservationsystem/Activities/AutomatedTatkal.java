034a7aeca7e693b4df2c746a70ac4f754f809a11
abhinay9955 <kumarabhinay599@gmail.com>
2020-02-08 16:53:38 +0530

    Automated Tatkal Button Added

---
+++ app/src/main/java/com/sih2020/railwayreservationsystem/Activities/AutomatedTatkal.java
@@ -0,0 +1,14 @@
+package com.sih2020.railwayreservationsystem.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AutomatedTatkal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_automated_tatkal);
+    }
+}
---
+++ app/src/main/java/com/sih2020/railwayreservationsystem/Services/TatkalService.java
@@ -0,0 +1,16 @@
+package com.sih2020.railwayreservationsystem.Services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class TatkalService extends Service {
+    public TatkalService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
