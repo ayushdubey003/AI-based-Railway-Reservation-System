15c6b898bc7ad460ca574d92f12852cb94c1e19c
tachodril <rajlevel97@gmail.com>
2020-02-08 22:03:56 +0530

    booking form basic layout

--- app/src/main/java/com/sih2020/railwayreservationsystem/Activities/AutomatedTatkal.java
+++ app/src/main/java/com/sih2020/railwayreservationsystem/Activities/AutomatedTatkal.java
@@ -6,13 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import com.sih2020.railwayreservationsystem.R;
 import com.sih2020.railwayreservationsystem.Services.TatkalService;
+import com.sih2020.railwayreservationsystem.Utils.GenerateBackground;
 
 public class AutomatedTatkal extends AppCompatActivity {
 
-    Button mService;
+    private Button mService;
+    private RelativeLayout appBar;
+    private Button reviewButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,16 +24,19 @@ public class AutomatedTatkal extends AppCompatActivity {
         setContentView(R.layout.activity_automated_tatkal);
         init();
 
-        mService.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startService(new Intent(AutomatedTatkal.this, TatkalService.class));
-            }
-        });
+//        mService.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startService(new Intent(AutomatedTatkal.this, TatkalService.class));
+//            }
+//        });
     }
 
-    public void init()
-    {
-        mService=findViewById(R.id.service);
+    public void init() {
+        //mService=findViewById(R.id.service);
+        appBar = findViewById(R.id.app_bar_at);
+        appBar.setBackground(GenerateBackground.generateBackground());
+        reviewButton=findViewById(R.id.review_button_at);
+        reviewButton.setBackground(GenerateBackground.generateBackground());
     }
 }
