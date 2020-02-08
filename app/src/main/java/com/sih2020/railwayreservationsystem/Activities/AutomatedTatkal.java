8865fb1010eb534be6ec294bd24afcecc7140b2b
tachodril <rajlevel97@gmail.com>
2020-02-08 23:30:21 +0530

    add passenger fragment added

--- app/src/main/java/com/sih2020/railwayreservationsystem/Activities/AutomatedTatkal.java
+++ app/src/main/java/com/sih2020/railwayreservationsystem/Activities/AutomatedTatkal.java
@@ -1,28 +1,38 @@
 package com.sih2020.railwayreservationsystem.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
+import com.sih2020.railwayreservationsystem.Fragments.AddPassengerFragment;
 import com.sih2020.railwayreservationsystem.R;
 import com.sih2020.railwayreservationsystem.Services.TatkalService;
+import com.sih2020.railwayreservationsystem.Utils.AppConstants;
 import com.sih2020.railwayreservationsystem.Utils.GenerateBackground;
 
 public class AutomatedTatkal extends AppCompatActivity {
 
     private Button mService;
+    private ScrollView layerToHide;
     private RelativeLayout appBar;
     private Button reviewButton;
+    private TextView addNewPassenger;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_automated_tatkal);
         init();
+        receiveClicks();
 
 //        mService.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -32,11 +42,38 @@ public class AutomatedTatkal extends AppCompatActivity {
 //        });
     }
 
+    private void receiveClicks() {
+        addNewPassenger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AppConstants.showBottomSheet(AutomatedTatkal.this);
+                AppConstants.mScrollView = findViewById(R.id.zeroth_layer);
+                layerToHide.setAlpha(0.1f);
+//                AddPassengerFragment bottomSheet = new AddPassengerFragment();
+//                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+//                transaction.setCustomAnimations(R.anim.slide_in_bottom, R.anim.no_anim);
+//                transaction.add(R.id.fragment_layout, bottomSheet);
+//                transaction.commit();
+            }
+        });
+    }
+
     public void init() {
         //mService=findViewById(R.id.service);
+        layerToHide = findViewById(R.id.zeroth_layer);
+        addNewPassenger = findViewById(R.id.add_new_passenger);
         appBar = findViewById(R.id.app_bar_at);
         appBar.setBackground(GenerateBackground.generateBackground());
-        reviewButton=findViewById(R.id.review_button_at);
+        reviewButton = findViewById(R.id.review_button_at);
         reviewButton.setBackground(GenerateBackground.generateBackground());
+
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                if(!AppConstants.mBottomSheetOpen){
+//                    layerToHide.setAlpha(1.0f);
+//                }
+//            }
+//        },500);
     }
 }
--- app/src/main/java/com/sih2020/railwayreservationsystem/Utils/AppConstants.java
+++ app/src/main/java/com/sih2020/railwayreservationsystem/Utils/AppConstants.java
@@ -3,10 +3,18 @@ package com.sih2020.railwayreservationsystem.Utils;
 import android.content.Context;
 import android.util.DisplayMetrics;
 import android.util.Pair;
+import android.widget.ScrollView;
 
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.sih2020.railwayreservationsystem.Activities.AutomatedTatkal;
+import com.sih2020.railwayreservationsystem.Fragments.AddPassengerFragment;
 import com.sih2020.railwayreservationsystem.Models.SpinnerModel;
 import com.sih2020.railwayreservationsystem.Models.Station;
 import com.sih2020.railwayreservationsystem.Models.Train;
+import com.sih2020.railwayreservationsystem.R;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -34,10 +42,12 @@ public class AppConstants {
     public static boolean mUseDate = true;
     public static ArrayList<Train> mTrainList = new ArrayList<>();
     public static String[] mNationality = {"Indian", "Others"};
-    public static String[] mOccupation = {"NA","Govt-employee","Farmer", "Self-employed","Unemployed"};
-    public static String[] mMaritalStatus = {"NA","Single", "Married"};
+    public static String[] mOccupation = {"NA", "Govt-employee", "Farmer", "Self-employed", "Unemployed"};
+    public static String[] mMaritalStatus = {"NA", "Single", "Married"};
     public static ArrayList<Pair<String, String>> mTrainListAtStartTime = new ArrayList<>();
-    public static Pair<String,String> mSpotTrainPair = new Pair<String,String>(null,null);
+    public static Pair<String, String> mSpotTrainPair = new Pair<String, String>(null, null);
+    public static boolean mBottomSheetOpen = false;
+    public static ScrollView mScrollView;
 
     public static float convertDpToPixel(float dp, Context context) {
         return dp * ((float) context.getResources().getDisplayMetrics().densityDpi / DisplayMetrics.DENSITY_DEFAULT);
@@ -45,5 +55,23 @@ public class AppConstants {
 
     public static float convertPixelsToDp(float px, Context context) {
         return px / ((float) context.getResources().getDisplayMetrics().densityDpi / DisplayMetrics.DENSITY_DEFAULT);
+    }
+
+    public static void showBottomSheet(AutomatedTatkal automatedTatkal) {
+        AddPassengerFragment bottomSheet = new AddPassengerFragment();
+        FragmentTransaction transaction = automatedTatkal.getSupportFragmentManager().beginTransaction();
+        transaction.setCustomAnimations(R.anim.slide_in_bottom, R.anim.no_anim);
+        transaction.add(R.id.fragment_layout, bottomSheet);
+        transaction.commit();
+        mBottomSheetOpen = true;
+    }
+
+    public static void hideBottomSheet(FragmentActivity activity) {
+        Fragment f = activity.getSupportFragmentManager().findFragmentById(R.id.fragment_layout);
+        FragmentTransaction transaction = activity.getSupportFragmentManager().beginTransaction();
+        transaction.setCustomAnimations(R.anim.no_anim, R.anim.slide_out_bottom);
+        transaction.remove(f).commit();
+        mBottomSheetOpen = false;
+        mScrollView.setAlpha(1.0f);
     }
 }
