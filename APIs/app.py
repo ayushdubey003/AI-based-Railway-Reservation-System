5ec3ccfc58c1608f579dd05d427667efdd63752e
Ayush Dubey <2017UGCS079@nitjsr.ac.in>
2020-02-09 23:18:19 +0530

    Blockchain Wallet Complete

--- APIs/app.py
+++ APIs/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import requests
 from bs4 import BeautifulSoup
 import csv
@@ -10,6 +10,9 @@ from sklearn.preprocessing import StandardScaler # install
 from datetime import datetime
 import os
 import sys
+import json
+from hashlib import sha256
+import time
 
 app = Flask(__name__)
 
@@ -622,6 +625,199 @@ def livestatus(trainno,doj):
         return (jsonify(error = str(e)))
     return jsonify(status = ans)
 
+
+class Block:
+    def __init__(self, index, transactions, timestamp, previous_hash, nonce=0):
+        self.index = index
+        self.transactions = transactions
+        self.timestamp = timestamp
+        self.previous_hash = previous_hash
+        self.nonce = nonce
+
+    def compute_hash(self):
+        block_string = json.dumps(self.__dict__, sort_keys=True)
+        return sha256(block_string.encode()).hexdigest()
+
+
+class Blockchain:
+    difficulty = 4
+
+    def __init__(self):
+        self.unconfirmed_transactions = []
+        self.chain = []
+
+    def create_genesis_block(self):
+        genesis_block = Block(0, [], 0, "0")
+        genesis_block.hash = genesis_block.compute_hash()
+        try:
+            self.chain = joblib.load("../datasets/wallet.pkl")
+            print(self.chain)
+        except Exception as e:
+            self.chain.append(genesis_block)
+            joblib.dump(self.chain,"../datasets/wallet.pkl")
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+    def add_block(self, block, proof):
+        previous_hash = self.last_block.hash
+
+        if previous_hash != block.previous_hash:
+            return False
+
+        if not Blockchain.is_valid_proof(block, proof):
+            return False
+
+        block.hash = proof
+        self.chain.append(block)
+        joblib.dump(self.chain,"../datasets/wallet.pkl")
+
+        return True
+
+    def proof_of_work(self, block):
+        block.nonce = 0
+
+        computed_hash = block.compute_hash()
+        while not computed_hash.startswith('0' * Blockchain.difficulty):
+            block.nonce += 1
+            computed_hash = block.compute_hash()
+
+        return computed_hash
+
+    def add_new_transaction(self, transaction):
+        self.unconfirmed_transactions.append(transaction)
+
+    @classmethod
+    def is_valid_proof(cls, block, block_hash):
+        return (block_hash.startswith('0' * Blockchain.difficulty) and
+                block_hash == block.compute_hash())
+
+    @classmethod
+    def check_chain_validity(cls, chain):
+        result = True
+        previous_hash = "0"
+
+        for block in chain:
+            block_hash = block.hash
+            delattr(block, "hash")
+
+            if not cls.is_valid_proof(block, block.hash) or \
+                    previous_hash != block.previous_hash:
+                result = False
+                break
+
+            block.hash, previous_hash = block_hash, block_hash
+
+        return result
+
+    def mine(self):
+        if not self.unconfirmed_transactions:
+            return False
+
+        last_block = self.last_block
+
+        new_block = Block(index=last_block.index + 1,
+                          transactions=self.unconfirmed_transactions,
+                          timestamp=time.time(),
+                          previous_hash=last_block.hash)
+
+        proof = self.proof_of_work(new_block)
+        self.add_block(new_block, proof)
+        print(len(self.chain))
+        self.unconfirmed_transactions = []
+        return new_block.index
+
+blockchain = Blockchain()
+blockchain.create_genesis_block()
+
+@app.route('/new_transaction', methods=['POST'])
+def new_transaction():
+    tx_data = request.get_json()
+    required_fields = ["sender", "receiver", "amount"]
+
+    for field in required_fields:
+        if not tx_data.get(field):
+            return jsonify(error=404)
+
+    tx_data["timestamp"] = time.time()
+
+    blockchain.add_new_transaction(tx_data)
+    return jsonify(success=200)
+
+@app.route('/chain', methods=['GET'])
+def get_chain():
+    chain_data = []
+    for block in blockchain.chain:
+        chain_data.append(block.__dict__)
+    return json.dumps({"length": len(chain_data),
+                       "chain": chain_data,})
+
+@app.route('/calculateWalletAmount/<uid>',methods=['GET'])
+def calculate_amount(uid):
+    chain_data = []
+    amount = 0
+    for block in blockchain.chain:
+        chain_data.append(block.__dict__)
+    for data in chain_data:
+        try:
+            if data['transactions'][0]['sender'] == uid:
+                amount = amount - data['transactions'][0]['amount']
+        except Exception as e:
+            continue
+        try:
+            if data['transactions'][0]['receiver'] == uid:
+                amount = amount + data['transactions'][0]['amount']
+        except Exception as e:
+            continue
+
+    return (jsonify(walletAmount = amount))
+
+@app.route('/mine', methods=['GET'])
+def mine_unconfirmed_transactions():
+    result = blockchain.mine()
+    if not result:
+        return "No transactions to mine"
+    return jsonify(Success="true")
+
+def create_chain_from_dump(chain_dump):
+    generated_blockchain = Blockchain()
+    generated_blockchain.create_genesis_block()
+    for idx, block_data in enumerate(chain_dump):
+        if idx == 0:
+            continue 
+        block = Block(block_data["index"],
+                      block_data["transactions"],
+                      block_data["timestamp"],
+                      block_data["previous_hash"],
+                      block_data["nonce"])
+        proof = block_data['hash']
+        added = generated_blockchain.add_block(block, proof)
+        if not added:
+            raise Exception("The chain dump is tampered!!")
+    return generated_blockchain
+
+@app.route('/add_block', methods=['POST'])
+def verify_and_add_block():
+    block_data = request.get_json()
+    block = Block(block_data["index"],
+                  block_data["transactions"],
+                  block_data["timestamp"],
+                  block_data["previous_hash"],
+                  block_data["nonce"])
+
+    proof = block_data['hash']
+    added = blockchain.add_block(block, proof)
+
+    if not added:
+        return jsonify(code=400)
+
+    return jsonify(code=200)
+
+
+@app.route('/pending_tx')
+def get_pending_tx():
+    return json.dumps(blockchain.unconfirmed_transactions)
 
 if __name__ == '__main__':
     app.run()
